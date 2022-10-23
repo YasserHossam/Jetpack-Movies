b1e79fb7b06475a0eeca93816b92d2e034a646bc
Yasser Hossam <yasserhossam.1994@gmail.com>
2022-10-23 13:19:19 +0200

    Add remote data source

---
+++ app/src/main/java/com/marvel/moviesapp/data/remote/MoviesRemoteDataSource.kt
@@ -0,0 +1,26 @@
+package com.marvel.moviesapp.data.remote
+
+import com.marvel.moviesapp.data.remote.model.RemoteMovie
+import com.marvel.moviesapp.data.remote.model.response.BaseListingResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface MoviesRemoteDataSource {
+
+    @GET("/movie/top_rated")
+    suspend fun getTopRatedMovies(@Query("page") page: Int): Response<BaseListingResponse<RemoteMovie>>
+
+    @GET("/movie/now_playing")
+    suspend fun getNowPlayingMovies(@Query("page") page: Int): Response<BaseListingResponse<RemoteMovie>>
+
+    @GET("/search/movie")
+    suspend fun getNowPlayingMovies(
+        @Query("query") query: String,
+        @Query("page") page: Int
+    ): Response<BaseListingResponse<RemoteMovie>>
+
+    @GET("/search/movie/{id}")
+    suspend fun getMovieDetails(@Path("id") id: Int): RemoteMovie
+}
