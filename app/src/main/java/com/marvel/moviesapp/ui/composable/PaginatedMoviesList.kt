af07734a5cfad7164fc505566e87eadadc1776ad
Yasser Hossam <yasserhossam.1994@gmail.com>
2022-10-25 20:20:57 +0200

    Add padding to show last row in the grid

--- app/src/main/java/com/marvel/moviesapp/ui/composable/PaginatedMoviesList.kt
+++ app/src/main/java/com/marvel/moviesapp/ui/composable/PaginatedMoviesList.kt
@@ -1,10 +1,14 @@
 package com.marvel.moviesapp.ui.composable
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
 import com.marvel.moviesapp.ui.model.MovieModel
 
@@ -17,7 +21,7 @@ fun PaginatedMoviesList(
     isFavoritesScreen: Boolean,
     onItemClick: (Int) -> Unit
 ) {
-    LazyVerticalGrid(modifier = modifier, cells = GridCells.Fixed(3)) {
+    LazyVerticalGrid(modifier = modifier.padding(bottom = 70.dp), cells = GridCells.Fixed(3)) {
         items(list.itemCount) { index ->
             list[index]?.let { MovieItem(movieModel = it, favoriteState, isFavoritesScreen, onItemClick) }
         }
